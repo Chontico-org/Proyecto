0e4372bf775cfd4bcce78e9566cbd64cac5a86f1
Andres-ZED <gilman.junior27@gmail.com>
2025-11-18 22:13:22 -0500

    nuevo1.2

---
+++ Proyecto_de_micros/.pio/build/adafruit_itsybitsy_esp32/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/adafruit_itsybitsy_esp32/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/gilma/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/gilma/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
